4c1ab92e2e7b8b3baf7f36fd68d3673a66f78647
markoblazevski <marko_blazevski@live.com>
2026-04-01 23:07:03 +0200

    docker compose files

---
+++ inventory-employees/src/test/java/com/markoblazhevski/inventoryemployees/InventoryEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markoblazhevski.inventoryemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryEmployeesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
